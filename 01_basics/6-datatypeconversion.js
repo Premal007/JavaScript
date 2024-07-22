39662be2c09f028f3930d3d591d6508a40b989d0
Premal007 <163706856+Premal007@users.noreply.github.com>
2024-07-22 12:21:50 +0000

    conversion

---
+++ 01_basics/6-datatypeconversion.js
@@ -0,0 +1,74 @@
+/*
+let score1 = 21;
+console.log(typeof score1);
+
+let score2 = "21";
+console.log(typeof (score2));
+
+let val = Number(score1);
+console.log(val); 
+*/
+
+/*
+let a = "20abc";
+let b = Number(a);
+console.log(typeof(b));
+console.log(b);
+*/
+/*
+let a = null;
+let b = Number(a);
+console.log(typeof(b));
+console.log(b);
+*/
+/*
+let a = undefined;
+let b = Number(a);
+console.log(typeof(b));
+console.log(b);
+*/
+/*
+let a = true;
+let b = Number(a);
+console.log(typeof(b));
+console.log(b);
+*/
+
+
+/*conversion in number*/
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+/////////////////////////////////////////////////
+/*
+let isloggedin = 1;
+let convert1 = Boolean(isloggedin);
+console.log(convert1);   // true
+
+let isloggedout = 0;
+let convert2 = Boolean(isloggedin);
+console.log(convert2);    // false
+*/
+
+/*
+let isloggedin = "";
+let convert1 = Boolean(isloggedin);
+console.log(convert1);
+
+let isloggedout = "premal";
+let convert2 = Boolean(isloggedin);
+console.log(convert2);
+*/
+
+// "" => false
+// "premal" => false
+
+/////////////////////////////////////////////////////////////
+/*
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+*/
